8fdce08e997277fcf3c1a441fa787f8d0b6ee269
annisaardhya <113829871+annisaardhya@users.noreply.github.com>
2022-10-17 20:47:50 +1100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,20 @@
+var vg_1 = "Map.vg.json";
+vegaEmbed("#map1",vg_1).then(function(result) {
+
+    //Access the vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_1 = "TOP10.vg.json";
+vegaEmbed("#barchart",vg_1).then(function(result) {
+
+    //Access the vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_1 = "AlcoholConsumption.vg.json";
+vegaEmbed("#scattered_plot1",vg_1).then(function(result) {
+
+    //Access the vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_1 = "HDI.vg.json";
+vegaEmbed("#scattered_plot2",vg_1). then(function(result) {
+
+    //Access the vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
